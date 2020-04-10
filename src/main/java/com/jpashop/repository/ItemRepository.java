90fa6253173e085713785a1bec49efd7bf3c3d86
mong83 <06007sk@gmail.com>
2020-04-11 07:20:18 +0900

    상품 도메인 개발

---
+++ src/main/java/com/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package com.jpashop.repository;
+
+import com.jpashop.domain.Member;
+import com.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null){
+            em.persist(item); //신규
+        }else {
+            em.merge(item);   //수정
+        }
+
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i" , Item.class)
+                .getResultList();
+    }
+}
